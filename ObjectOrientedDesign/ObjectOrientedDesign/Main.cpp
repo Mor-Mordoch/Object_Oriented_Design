c217aa3d06b4f8f3c447741f2f92b37b93307aff
MorMordoch94 <60321987+MorMordoch94@users.noreply.github.com>
2020-09-15 11:58:26 +0300

    builder theory added

--- ObjectOrientedDesign/ObjectOrientedDesign/Main.cpp
+++ ObjectOrientedDesign/ObjectOrientedDesign/Main.cpp
@@ -1,7 +1,29 @@
+#include "Builder.h"
 #include "Adapter.h"
 
 int main()
 {
+	std::cout << "/*---------*/\n";
+	std::cout << "/* Builder */\n";
+	std::cout << "/*---------*/\n";
+	NutritionFactsDirector hamburger_director;
+	hamburger_director.SetNutritionFactsBuilder(new HamburgerNutritionFactsBuilder());
+	hamburger_director.BuildFullNutritionFacts();
+	CNutritionFacts* hamburger = hamburger_director.GetNutritionFacts();
+	hamburger->ShowNutritionFacts();
+
+	std::cout << "\n";
+
+	NutritionFactsDirector milk_director;
+	INutritionFactsBuilder* milk_builder = new MilkNutritionFactsBuilder();
+	milk_director.SetNutritionFactsBuilder(milk_builder);
+	milk_director.BuildPartialNutritionFacts();
+	CNutritionFacts* milk = milk_director.GetNutritionFacts();
+	milk->ShowNutritionFacts();
+
+	std::cout << "/*---------*/\n";
+	std::cout << "/* Adapter */\n";
+	std::cout << "/*---------*/\n";
 	/* Adapter - Class adapter implementation */
 	Rectangle* r = new RectangleAdapter(5, 10);
 	std::cout << "Rectangle area is : " << r->GetArea() << "\n";
